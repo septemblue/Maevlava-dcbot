fc9ea149016d8b0b439afc9cb521f1186c44def6
septemblue <focusramxram@gmai.com>
2022-05-22 00:05:52 +0700

    add send to specific channel feature

--- dist/app.js
+++ dist/app.js
@@ -34,6 +34,14 @@ const client = new discord_js_1.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] }
 client.once("ready", () => {
     var _a;
     console.log(`Client Ready ${(_a = client.user) === null || _a === void 0 ? void 0 : _a.tag}!`);
+    // SEND TO SPECIFIC Channel
+    let channels = client.channels.cache.map((guildChannel) => {
+        if (guildChannel.type === "GUILD_TEXT") {
+            const textChannel = client.channels.cache.get(guildChannel.id);
+            textChannel.send("Hello");
+            console.log(textChannel.name);
+        }
+    });
 });
 // DM + EMBED
 client.on("messageCreate", (message) => {
@@ -41,7 +49,7 @@ client.on("messageCreate", (message) => {
         const introEmbed = new discord_js_1.default.MessageEmbed()
             .setTitle("Maevlava Bot")
             .setDescription("Personal bot for Maevlava")
-            .setFields({ name: "Functions", value: "1. DM\n2.Embed" })
+            .setFields({ name: "Functions", value: "1. DM\n2.Embed\n3. Send to Specific Channel" })
             .setImage("https://i.pinimg.com/564x/a7/46/fb/a746fb1edf92f4bc62809b548c3424f9.jpg");
         message.author.send({ embeds: [introEmbed] });
     }
